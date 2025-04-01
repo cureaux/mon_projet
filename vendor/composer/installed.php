cd4c118ca76905ba6d2ceae253d1e72685a08c12
cureaux <camron310303@gmail.com>
2025-04-01 16:01:24 +0200

    Ajout de l'action de vérification de l'arborescence

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cureaux/monprojet',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bd13c556e432592d2c2c28728f2ee36077639a60',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cureaux/monprojet' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bd13c556e432592d2c2c28728f2ee36077639a60',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.12.0',
+            'version' => '3.12.0.0',
+            'reference' => '2d1b63db139c3c6ea0c927698e5160f8b3b8d630',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
