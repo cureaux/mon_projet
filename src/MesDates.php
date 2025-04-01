4bb1cbbe202a941d3329fa3c6602013c1009ac75
cureaux <camron310303@gmail.com>
2025-04-01 14:48:49 +0200

    Ajout de l'action GitHub pour lister les fichiers sur push

--- src/MesDates.php
+++ src/MesDates.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Fichier principal.
+ * Ce fichier est utilisé pour instancier un objet de la classe MesDates
+ * et afficher la date de demain.
+ * php version 8.4.5
+ *
+ * @category Utility
+ * @package  DateFunctions
+ * @author   camron <camron310303@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://bloup.nvsphr.fr
+ */
+
 declare(strict_types=1);
 
 namespace UPJV;
